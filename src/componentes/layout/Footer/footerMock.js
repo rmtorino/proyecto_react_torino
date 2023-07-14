448fe7b61034e679e53e225d03ba584be8bcb5c1
Ricardo Torino <rmt.torino@gmail.com>
2023-07-14 20:10:43 -0300

    Ahora funciona todo, menos el spinner

--- src/componentes/layout/Footer/footerMock.js
+++ src/componentes/layout/Footer/footerMock.js
@@ -25,3 +25,18 @@ export const footerCard = [
     description: "Contacto",
   },
 ];
+
+export const footerSocialMedia = [
+  {
+    id: 1,
+    img: "https://res.cloudinary.com/defldhv32/image/upload/v1689376157/logo_instagram_vr61qo.jpg",
+  },
+  {
+    id: 1,
+    img: "https://res.cloudinary.com/defldhv32/image/upload/v1689376157/logo_whatsapp_szoyqt.jpg",
+  },
+  {
+    id: 1,
+    img: "https://res.cloudinary.com/defldhv32/image/upload/v1689376157/logo_facebook_tyoajy.jpg",
+  },
+];
